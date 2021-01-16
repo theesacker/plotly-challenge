03c62bb375b55299b322582d1a7b8b3552de3bad
Tyson Heesacker <tysonheesacker86@gmail.com>
2021-01-15 22:17:36 -0800

    saving before graphs

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 
 // // Create listener on dropdown
-// d3.select("#selDataset").on("click", handleSubmit);
-// function handleSubmit(){
-//     d3.event.preventDefault();
-// }
+d3.select("#selDataset").on("change", handleSubmit);
+function handleSubmit(){
+    d3.event.preventDefault();
+}
 
 function unpack(rows,index){
     return rows.map(function(row){
@@ -16,9 +16,28 @@ d3.json("data/samples.json").then(function(data){
 console.log(data);
 var names = data.names;
 console.log(names);
-// var sampleValues = unpack(data.names.samples.sample_values,1);
+
+for (var j = 0; j < data.names.length; j++){
+// console.log(names[j]);
+// create Dropdown of IDs
+d3.select("#selDataset").selectAll("option")
+.data(data.names)
+.enter()
+.append("option")
+.html(function(d) {
+    return `<option>${names[j]}</option>`;
+});
+}
+// var sampValues = data.samples;
+// var 
+// console.log(sampValues[1])
+
+});
+
+
+
+// var sampleValues = data.samples.id;
 // console.log(sampleValues);
 
-})
-// var names = bButtondata.names
-// console.log(names);
+// });
+
