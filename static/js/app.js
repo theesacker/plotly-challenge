9445226c81b7a2b08799100649acd9aa7a2d4d60
Tyson Heesacker <tysonheesacker86@gmail.com>
2021-01-16 11:09:51 -0800

    creating index in dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,38 @@
 
 // // Create listener on dropdown
 d3.select("#selDataset").on("change", handleSubmit);
-function handleSubmit(){
+function handleSubmit() {
     d3.event.preventDefault();
 }
 
-function unpack(rows,index){
-    return rows.map(function(row){
+function unpack(rows, index) {
+    return rows.map(function (row) {
         return row[index];
     });
 }
 
 
-d3.json("data/samples.json").then(function(data){
-console.log(data);
-var names = data.names;
-console.log(names);
+d3.json("data/samples.json").then(function (data) {
+    console.log(data);
+    var names = data.names;
+    console.log(names);
 
-for (var j = 0; j < data.names.length; j++){
-// console.log(names[j]);
-// create Dropdown of IDs
-d3.select("#selDataset").selectAll("option")
-.data(data.names)
-.enter()
-.append("option")
-.html(function(d) {
-    return `<option>${names[j]}</option>`;
-});
+    for (var i = 0; i < data.names.length; i++) {
+    for (var j = 0; j < data.names.length; j++) {
+        // console.log(names[j]);
+        // create Dropdown of IDs
+        d3.select("#selDataset").selectAll("option")
+            .data(data.names)
+            .enter()
+            .append("option")
+            .html(function (d) {
+                return `<option id="${i++}">${names[j++]}</option>`;
+            });
+    }
 }
-// var sampValues = data.samples;
-// var 
-// console.log(sampValues[1])
+    // var sampValues = data.samples;
+    // var 
+    // console.log(sampValues[1])
 
 });
 
